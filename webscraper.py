c4c317fc4ebe0b4ba7b1a3867fde0883cd1accdb
Szalony Bolo <vaksus@gmail.com>
2019-10-15 09:40:44 +0200

    INIT

--- webscraper.py
+++ webscraper.py
@@ -2,22 +2,48 @@
 import urllib
 import sys
 from pathlib import Path
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup`
 
 def getPage(address):
   html = urllib.request.urlopen(address).read()
   return html
 
-def createPLLink(card):
-  leftURL = "http://planeswalker.pl/szukaj?controller=search&orderby=position&orderway=desc&search_query="
-  rightURL = "&submit_search="
-  return createLink(card, leftURL, rightURL)
+def PPL:
+  def createLink(card):
+    leftURL = "http://planeswalker.pl/szukaj?controller=search&orderby=position&orderway=desc&search_query="
+    rightURL = "&submit_search="
+    return createLink(card, leftURL, rightURL)
 
-def createFGLink(card):
-  leftURL = ""
-  cardURL = urllib.parse.urlencode(card)
-  rightURL = ""
-  return createLink(card, leftURL, rightURL)
+  def parsePage(page):
+		soup = BeautifulSoup(page)
+  	containers = soup.find_all("div", class_="product-container")
+    result = []
+    for container in containers:
+      price = container.find("span", class_="product-price")
+			quantityGrid = container.find("span", class="qty_grid")
+			quantity = quantityGrid.text[1:]
+		  if quantity.endswith("+")
+			
+      if (quantity[:]
+
+def FG:
+  def createLink(card):
+    leftURL = ""
+    cardURL = urllib.parse.urlencode(card)
+    rightURL = ""
+    return createLink(card, leftURL, rightURL)
+  
+  def parsePage(page):
+		soup = BeautifulSoup(page)
+		tables = soup.find_all('table')
+		table = tables[1]
+    tds = table.find_all('td')
+    result = []
+    for td in tds:
+       if td.text.endswith('zl')
+         result.append(td.text[:-2])
+    return result
+
 
 def createLink(card, leftURL, rightURL):
   return leftURL + urllib.parse.urlencode(card) + rightURL
@@ -28,7 +54,14 @@ def parsePage(page):
   soup.find_all('')
 
 def searchAllEngines(card):
-  
+  engines = []
+	engines.append(PPL())
+	engines.append(FG))
+
+  for engine in engines:
+    link = engine.create(card)
+		page = getPage(link)
+    result = parsePage(page)
 
 def main():
   argLen = len(sys.argv)
