8d01d396af386a3a5bdaeb4446b269719a8189ac
Szalony Bolo <vaksus@gmail.com>
2019-11-17 19:59:33 +0100

    cards and proper version

---
+++ skapiec.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+import urllib
+import urllib.request
+import sys
+from pathlib import Path
+from bs4 import BeautifulSoup
+
+def getPage(address):
+  html = urllib.request.urlopen(address).read()
+  return html
+
+class PPL:
+  title = "Planeswalker"
+  def createLink(self, card):
+    leftURL = "http://planeswalker.pl/szukaj?controller=search&orderby=position&orderway=desc&search_query="
+    rightURL = "&submit_search="
+    return createLink(card, leftURL, rightURL)
+
+  def parsePage(self, page):
+    soup = BeautifulSoup(page, features="html.parser")
+    containers = soup.find_all("div", class_="product-container")
+    result = []
+    for container in containers:
+      price = container.find("span", class_="product-price")
+      quantityGrid = container.find("span", class_="qty_grid")
+      quantity = quantityGrid.text[1:]
+      if quantity.endswith("+"):
+        quantity = quantity[:-1]
+      if int(quantity) > 0:
+        result.append(price.text)
+      else:
+        result.append("BRAK")
+    return result
+
+class FG:
+  title = "Flamberg"
+  def createLink(self, card):
+    leftURL = "https://www.flamberg.com.pl/advanced_search_result.php?keywords="
+    return createLink(card, leftURL, "")
+
+  def parsePage(self, page):
+    soup = BeautifulSoup(page, features="html.parser")
+    tables = soup.find_all('table')
+    table = tables[1]
+    trs = table.find_all('tr')
+    result = []
+    for tr in trs:
+      tds = tr.find_all('td')
+      if len(tds) > 5:
+        price = tds[3].text
+        quantity = tds[4].text
+        if int(quantity) > 0:
+          result.append(price[:-2])
+        else:
+          result.append("BRAK")
+    return result
+
+def createLink(card, leftURL, rightURL):
+  return leftURL + urllib.parse.quote(str(card)) + rightURL
+
+# def parsePage(page):
+#   result = []
+#   soup = BeautifulSoup(page, features="html.parser")
+#   soup.find_all('')
+
+class EngineManager:
+
+  engines=[]
+
+  def __init__(self):
+    self.engines.append(FG())
+    self.engines.append(PPL())
+
+  def searchAllEngines(self, card):
+    searchResult = []
+    for engine in self.engines:
+      link = engine.createLink(card)
+      page = getPage(link)
+      result = engine.parsePage(page)
+      searchResult.append(result)
+    return searchResult
+
+  def displayTableHeader(self):
+    print("\t", end="")
+    for engine in self.engines:
+      print("\t" + engine.title, end="")
+    print("")
+
+def main():
+  argLen = len(sys.argv)
+  if argLen < 2:
+    print('You should pass card name or file with card list in first argument')
+    sys.exit()
+
+  eng = EngineManager()
+  my_file = Path(sys.argv[1])
+  if my_file.is_file():
+    f = open(str(my_file), "r")
+    lines = f.readlines()
+    eng.displayTableHeader()
+    for line in lines:
+        cardResult = eng.searchAllEngines(line)
+        print(line, end="")
+        for i in range(0, len(cardResult[0])):
+          #print("\t" + result, end="")
+          print("\t" + cardResult[0][i], end="")
+          print("\t" + cardResult[1][i], end="")
+          print("")
+        print("")
+    f.close()
+  else:
+    print("Can't open " + my_file)
+
+if __name__ == "__main__":
+  main()
